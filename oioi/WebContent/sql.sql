315c8d03328e81acf815244d76181f6e2623087a
verus0215 <verus747@naver.com>
2020-03-09 13:52:29 +0900

    test

--- oioi/WebContent/sql.sql
+++ oioi/WebContent/sql.sql
@@ -1,12 +1,11 @@
-drop table member;
+drop table user_member;
 
-create table member(
+create table user_member(
 	id varchar2(200) primary key,
 	pw varchar2(200),
 	name varchar2(200),
 	nation varchar2(200),
 	email varchar2(200)
-	
 )
 
 select * from member;
