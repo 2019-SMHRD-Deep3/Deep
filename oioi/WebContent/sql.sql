e6d5cbb7a719e9bd8c588bdbf03393c7cf181bb2
owonil <owonil0411@gmail.com>
2020-03-11 16:24:17 +0900

    하위~ 수정본

--- oioi/WebContent/sql.sql
+++ oioi/WebContent/sql.sql
@@ -9,9 +9,14 @@ create table user_member(
 
 select * from user_member;
 
+drop table user_member;
 
-
-
+create table user_member(
+	id varchar2(200) primary key,
+	pw varchar2(200),
+	name varchar2(200),
+	email varchar2(200)
+)
 
 
 
