23a3ae9e3548a606c380203877858c7c0012b5af
jini81 <jinieye@hanmail.net>
2020-03-12 11:55:58 +0900

    ICommand2삭제

--- oioi/src/front/ICommand2.java
+++
@@ -1,8 +0,0 @@
-package front;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ICommand2 {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
